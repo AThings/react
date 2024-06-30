6332097f87043f4f1f26650adc592c2f3b141b69
AThings <2846965655@qq.com>
2024-06-30 15:23:48 +0800

    chore: 新建react项目

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+// 根组件
+function App() {
+  return (
+    <div className="App">
+      this is App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// 项目入口 项目从这里开始运行
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
